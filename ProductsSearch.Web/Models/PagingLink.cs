eb798ff76f4daae0931956eedfeee7c597d8a77c
Pablo Alcaino <pablo.alcaino@arkanosoft.com>
2020-12-13 22:06:35 -0300

    Clean up

--- ProductsSearch.Web/Models/PagingLink.cs
+++ ProductsSearch.Web/Models/PagingLink.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ProductsSearch.Web.Models
+﻿namespace ProductsSearch.Web.Models
 {
+    /// <summary>
+    /// Pagination Link 
+    /// </summary>
     public class PagingLink
     {
+        /// <summary>
+        /// Indicates the Text of the Link
+        /// </summary>
         public string Text { get; set; }
+
+        /// <summary>
+        /// The Current page of the pagination stack
+        /// </summary>
         public int Page { get; set; }
+
+        /// <summary>
+        /// Determines if this Paging Link is enabled, otherwise, false.
+        /// </summary>
         public bool Enabled { get; set; }
+
+        /// <summary>
+        /// Determines if this Paging Link is active, otherwise, false.
+        /// </summary>
         public bool Active { get; set; }
+
         public PagingLink(int page, bool enabled, string text)
         {
             Page = page;
